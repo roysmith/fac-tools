c0a82d6c78b7f0b37194495731f00d39b6c404f8
Roy Smith <roy@panix.com>
2026-03-17 19:56:57 -0400

    Add fac() fixture

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -1,10 +1,27 @@
-import mwparserfromhell as mwp
+from pathlib import Path
+
+from mwparserfromhell.wikicode import Wikicode
+import pytest
 
 from fac_tools import Archive
 
-def test_parse(datadir):
-    path = datadir  / 'Wikipedia:Featured_article_candidates/Crusading_movement/archive2@1343969032'
-    text = path.open().read()
+@pytest.fixture
+def fac(datadir: Path):
+    """Return the wikitext of a FAC archive page.  The wikitext
+    must already be stored in the test_archive directory.
+
+    """
+
+    def _fac(archive_name) -> Wikicode:
+        path = datadir / "Wikipedia:Featured_article_candidates" / archive_name
+        with path.open() as f:
+            return f.read()
+    
+    return _fac
+
+def test_build(fac):
+    text = fac("Crusading_movement/archive2@1343969032")
     archive = Archive.build(text)
     assert isinstance(archive, Archive)
-    assert isinstance(archive.wikicode, mwp.wikicode.Wikicode)
+    assert isinstance(archive.wikicode, Wikicode)
+    assert "===[[Crusading movement]]===" in archive.wikicode
