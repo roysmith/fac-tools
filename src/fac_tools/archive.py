56b64ed7e17d89cbb62b07fb3a06b45f285bfb2b
Roy Smith <roy@panix.com>
2026-03-18 11:26:55 -0400

    Add support_count(), oppose_count()

--- src/fac_tools/archive.py
+++ src/fac_tools/archive.py
@@ -2,12 +2,35 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 import mwparserfromhell as mwp
+from mwparserfromhell.wikicode import Wikicode, Tag, Node
+
 
 
 @dataclass(frozen=True)
 class Archive:
-    wikicode: mwp.wikicode.Wikicode
+    wikicode: Wikicode
 
     @staticmethod
     def build(text: str) -> Archive:
         return Archive(mwp.parse(text))
+
+    def _isbold(self, node: Node) -> bool:
+        "Return True if node is a bit of bolded text"
+        parent = self.wikicode.get_parent(node)
+        return isinstance(parent, Tag) and parent.tag == "b"
+
+    def support_count(self) -> int:
+        return self._vote_count("support")
+
+    def oppose_count(self) -> int:
+        return self._vote_count("oppose")
+
+    def _vote_count(self, word) -> int:
+        count = 0
+        for h in self.wikicode.filter_headings():
+            if word.lower() in h.title.lower():
+                count += 1
+        for t in self.wikicode.filter_text():
+            if word.lower() in t.value.lower() and self._isbold(t):
+                count += 1
+        return count
--- tests/test_archive.py
+++ tests/test_archive.py
@@ -8,11 +8,14 @@ from fac_tools import Archive
 
 @pytest.fixture
 def fac(datadir: Path):
-    """Return the wikitext of a FAC archive page.  The wikitext
-    must already be stored in the test_archive directory.
-    """
+    def _fac(archive_name: str) -> Wikicode:
+        """Return the wikitext of a FAC archive page.  The wikitext
+        must already be stored in the test_archive directory.
 
-    def _fac(archive_name) -> Wikicode:
+        Archive_name is the nomination page name with the leading
+        'Wikipedia:Featured article candidates' stripped and '@<revision id>'
+        tacked on the end, and then url-encoded.
+        """
         path = datadir / "Wikipedia:Featured_article_candidates" / archive_name
         with path.open() as f:
             return f.read()
@@ -26,3 +29,15 @@ def test_build(fac):
     assert isinstance(archive, Archive)
     assert isinstance(archive.wikicode, Wikicode)
     assert "===[[Crusading movement]]===" in archive.wikicode
+
+
+def test_supports(fac):
+    archive = Archive.build(fac("Crusading_movement/archive2@1343969032"))
+    assert archive.support_count() == 3
+
+
+def test_opposes(fac):
+    archive = Archive.build(
+        fac("Serpent_labret_with_articulated_tongue/archive1@1343867890")
+    )
+    assert archive.oppose_count() == 1
