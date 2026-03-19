9855bfd78348db8323bb8fb3d6e5595baca7e298
Roy Smith <roy@panix.com>
2026-03-19 09:43:53 -0400

    Import UTC directly

--- tests/test_revision.py
+++ tests/test_revision.py
@@ -1,10 +1,9 @@
-from datetime import datetime, timezone
-
+from datetime import datetime, UTC
 
 from fac_tools import Revision
 
 
 def test_construct():
-    rev = Revision(datetime(2026, 1, 1, tzinfo=timezone.utc), "RoySmith")
-    assert rev.timestamp == datetime(2026, 1, 1, tzinfo=timezone.utc)
+    rev = Revision(datetime(2026, 1, 1, tzinfo=UTC), "RoySmith")
+    assert rev.timestamp == datetime(2026, 1, 1, tzinfo=UTC)
     assert rev.username == "RoySmith"
