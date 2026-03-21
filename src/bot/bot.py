3612e738ba0a754d21fd806a8fbf04c17678db34
Roy Smith <roy@panix.com>
2026-03-20 22:11:30 -0400

    MVP of output line

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,21 +1,48 @@
-from fac_tools import Nomination, Revision
+from humanize import naturaltime, naturaldelta
+
+import mwparserfromhell as mwp
 from pywikibot import Site, Page
+
+from fac_tools import Nomination, Revision
 
 
 def main():
     site = Site("en", "wikipedia")
-    page = Page(
-        site, "Wikipedia:Featured article candidates/Crusading movement/archive2"
+    nomination_page = (
+        "Wikipedia:Featured article candidates/Crusading movement/archive2"
     )
-    nomination = build_nomination(page)
-    print(nomination)
+    page = Page(site, nomination_page)
+
+    pwb_revisions = list(page.revisions(content=False))
+    last_timestamp = pwb_revisions[0]["timestamp"]
+    last_rev = list(page.revisions(total=1, starttime=last_timestamp, content=True))[0]
+    text = last_rev.text
+    fac_revisions = [Revision(r.timestamp, r.user) for r in pwb_revisions]
+
+    nom = Nomination(mwp.parse(text), nomination_page, fac_revisions)
+    print(
+        f"[[{nom.title()}]]"
+        f" ("
+        f"[[{nom.nomination}|nomination]]{{{{cdot}}}}"
+        f"{naturaldelta(nom.age())} ago{{{{cdot}}}}"
+        f"Active {naturaldelta(nom.active())} ago{{{{cdot}}}}"
+        f"{plural(len(nom.nominators()), 'nominator', 'nominators')}{{{{cdot}}}}"
+        f"{plural(len(nom.editors()), 'participant', 'participants')}{{{{cdot}}}}"
+        f"{plural(nom.support_count(), 'support', 'supports')}{{{{cdot}}}}"
+        f"{plural(nom.oppose_count(), 'oppose', 'opposes')}"
+        f")"
+    )
 
 
-def build_nomination(page: Page) -> Nomination:
-    fac_revs = []
-    for pwb_rev in page.revisions():
-        fac_revs.append(Revision(pwb_rev.timestamp, pwb_rev.user))
-    return Nomination.build(page.get(), fac_revs)
+def plural(n: int, singular: str, plural: str) -> str:
+    return f"{n} {singular if n == 1 else plural}"
+
+
+# def build_nomination(page: Page) -> Nomination:
+#     fac_revs = []
+#     for pwb_rev in page.revisions():
+#         fac_revs.append(Revision(pwb_rev.timestamp, pwb_rev.user))
+#     return Nomination.build(page.get(), fac_revs)
 
 
 if __name__ == "__main__":
