50914d032417744071f64859d6f40ac1be99de5d
Roy Smith <roy@panix.com>
2026-03-22 23:37:04 -0400

    Add sample-user-config.py and notes

---
+++ toolforge/sample-user-config.py
@@ -0,0 +1,12 @@
+# Sample pywikibot user-config.py
+# Make a copy of this, ensure that it is mode 0600,
+# and insert the appropriate OAuth credentials.  Do
+# NOT check these credentials into version control
+
+usernames["wikipedia"]["en"] = "FACSummaryBot"
+authenticate["en.wikipedia.org"] = (
+    "xxxx",  # consumer_key
+    "xxxx",  # consumer_secret
+    "xxxx",  # access_key
+    "xxxx",  # access_secret
+)
