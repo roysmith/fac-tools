011514fe2d9b93189762859ffe50b571006bd907
Roy Smith <roy@panix.com>
2026-03-24 18:47:18 -0400

    Add article size to summary

--- src/fac_tools/scripts/fac_summary_bot.py
+++ src/fac_tools/scripts/fac_summary_bot.py
@@ -2,13 +2,16 @@ from argparse import ArgumentParser, BooleanOptionalAction
 from collections.abc import Iterable
 from datetime import datetime
 from io import StringIO
+import requests
 from time import sleep
 
+from bs4 import BeautifulSoup
 import humanize
+from more_itertools import flatten
 import mwparserfromhell as mwp
 from pywikibot import Site, Page
 
-from fac_tools import Nomination, Revision
+from fac_tools import Nomination, Revision, FAToolsError
 
 INDEX_PAGE = "Wikipedia:Featured article candidates"
 SUMMARY_PAGE = "User:FACSummaryBot/summary"
@@ -82,8 +85,19 @@ def process_section(nominations: list[Nomination], buffer: StringIO):
 def process_nomination(nom: Nomination, now: datetime, buffer: StringIO):
     age = nom.creation_time() - now
     active = nom.last_edit_time() - now
-    article = Page(site, nom.title())
+    article = None
+    try:
+        article = Page(site, nom.title())
+    except FAToolsError:
+        # This probably means the nomination has already been
+        # promoted or archived.  We should probaby do something
+        # more useful here.
+        return
     description = get_short_description(article)
+    url = article.full_url()
+    response = requests.get(url, headers={"user-agent": "fac-tools (RoySmith)"})
+    size = get_prose_size(response.text)
+    rounded_size = humanize.intcomma(round(size / 1000.0) * 1000)
 
     # Isn't this why jinja templates were invented?
     buffer.write(f"<big>'''[[{nom.title()}]]'''</big>")
@@ -92,9 +106,11 @@ def process_nomination(nom: Nomination, now: datetime, buffer: StringIO):
         buffer.write(f": {humanize.ordinal(nom.archive_number())}")
     buffer.write(f")\n")
     if description:
-        buffer.write(f"* ''{description}''\n")
+        buffer.write(f"* ''{description}''")
     else:
-        buffer.write(f"* no {{{{t|short description}}}} found\n")
+        buffer.write(f"* no {{{{t|short description}}}} found")
+    buffer.write(f"{{{{cdot}}}}")
+    buffer.write(f"{rounded_size} words\n")
     buffer.write(f"* {humanize.naturaldelta(age)} old")
     buffer.write(f"{{{{cdot}}}}")
     buffer.write(f"Active {humanize.naturaldelta(active)} ago\n")
@@ -139,6 +155,21 @@ def get_short_description(article: Page) -> str:
     wikicode = mwp.parse(article.text)
     templates = wikicode.filter_templates(matches="short description")
     return templates and templates[0].get(1, "")
+
+
+def get_prose_size(text: str) -> int:
+    """Return an estimate of the readable prose size of the
+    page.  In theory, this should produce the same value as
+    [[Wikipedia:Prosesize]] but it's not clear exactly what
+    algorithm that uses, so this is only going to be roughly
+    the same.
+    """
+    soup = BeautifulSoup(text, features="html.parser")
+    paragraphs = soup.select("#mw-content-text p")
+    strings = flatten(p.stripped_strings for p in paragraphs)
+    tokens = flatten(s.split() for s in strings)
+    words = [t for t in tokens if t.isalpha()]
+    return len(words)
 
 
 if __name__ == "__main__":
---
+++ tests/scripts/test_fac_summary_bot.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+from more_itertools import flatten
+import pytest
+
+from fac_tools.scripts import fac_summary_bot
+
+
+@pytest.mark.parametrize(
+    "html,expected",
+    [
+        # Sizes reported by [[Wikipedia:Prosesize]]; our target
+        # is +/- 15% of that.
+        ("Chris Redfield@1344829095.html", 2837),
+        ("Tomb of Kha and Merit@1344360978.html", 8306),
+    ],
+)
+def test_article_size(datadir: Path, html: str, expected: int):
+    path = datadir / html
+    text = None
+    with path.open() as f:
+        text = f.read()
+    word_count = fac_summary_bot.get_prose_size(text)
+    assert abs(word_count - expected) <= expected * 0.15
